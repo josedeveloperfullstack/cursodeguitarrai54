fb48ef77f274c29059a2acb92cf4c143bfba87e1
Jose Alfredo Aguado <jose.aguado@misena.edu.co>
2024-03-18 14:39:20 -0500

    funciones de botones

--- javascript.js
+++ javascript.js
@@ -1,7 +1,25 @@
+
+
+
+
+
+document.querySelectorAll('.nav-button2').forEach(function(button) {
+    button.addEventListener('click', function() {
+        let divDestino = document.querySelector('.parrafo'); // Selecciona el primer div con la clase 'benefits'
+        divDestino.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.querySelectorAll('.nav-button3').forEach(function(button) {
+    button.addEventListener('click', function() {
+        let divDestino = document.querySelector('.contact'); // Selecciona el primer div con la clase 'benefits'
+        divDestino.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 document.querySelectorAll('.nav-button').forEach(function(button) {
     button.addEventListener('click', function() {
         window.scrollTo(0, 0);
         location.reload(); // Esto recarga la página
     });
 });
-
